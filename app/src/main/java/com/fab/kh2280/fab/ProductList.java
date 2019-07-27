2119910b26de2d68ebc0d932aea8fb5edde3262f
anshul.goel <anshul.goel@kony.com>
2019-07-27 17:11:16 +0530

    Added Firebase

--- app/src/main/java/com/fab/kh2280/fab/ProductList.java
+++ app/src/main/java/com/fab/kh2280/fab/ProductList.java
@@ -4,9 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
 
 public class ProductList extends Activity {
 
@@ -36,6 +45,15 @@ public class ProductList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        myRef.setValue("Hello, Country!");
+        myRef.setValue("Hello, State!");
+        myRef.setValue("Hello, City!");
+
         //Set Custom Adapter to list
         CustomProductListAdaptor whatever = new CustomProductListAdaptor(this, nameArray, infoArray, imageArray);
         listView = (ListView) findViewById(R.id.product_list_item);
@@ -52,7 +70,27 @@ public class ProductList extends Activity {
                 startActivity(intent);
             }
         });
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
     }
+
+
 
 
 }
